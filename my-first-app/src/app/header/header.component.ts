5ee37f834ca35dedb5da9f254d50e8cde02bfa6f
IBERMATICA\BICUGUAL <a.cuesta.gutierrez@ibermatica.com>
2023-01-19 15:56:35 +0100

    Correccion de errores en la busqueda de heroes

--- my-first-app/src/app/header/header.component.ts
+++ my-first-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { HeaderService } from '../header.service';
 
 @Component({
@@ -8,12 +9,19 @@ import { HeaderService } from '../header.service';
 })
 export class HeaderComponent {
 
-  @Output()
-  searchEvent = new EventEmitter<string>();
-
-  constructor(private headerService: HeaderService) { }
+  constructor(
+    private headerService: HeaderService,
+    private router: Router) { }
 
   search(searchValue: string): void {
+    
+    //Comprobacion de la ruta activa
+    if (this.router.url !== '/dashboard'){
+      //Si la pagina no es la de dasboard
+      alert('El filtrado de héroes solo funciona en la pagina Inicial');
+    }
+      console.log(this.router.url);
+    
     console.log(`SOLICITUD DE BÚSQUEDA --> seachValue: ${searchValue}`);
     this.headerService.search(searchValue);
   }
--- my-first-app/src/app/heroes/heroes.component.ts
+++ my-first-app/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HeaderService } from '../header.service';
 import { HeroServiceService } from '../hero-service.service';
 import { Hero } from './../model/hero';
@@ -10,12 +11,12 @@ import { Hero } from './../model/hero';
 })
 export class HeroesComponent implements OnInit {
   heroes!: Hero[];
-  constructor(private heroService: HeroServiceService, private headerService: HeaderService) {}
+
+  constructor(private heroService: HeroServiceService) { }
 
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
     this.getHeroes();
-    this.headerService.searchEvent.subscribe( (searchValue: string) => alert('El buscador no funciona en esta pantalla'));
   }
 
   getHeroes() {
@@ -23,14 +24,15 @@ export class HeroesComponent implements OnInit {
     //   this.heroes = heroes
     // );
 
-    this.heroService.getAllHeroes().subscribe( heroes =>
+    this.heroService.getAllHeroes().subscribe(heroes =>
       this.heroes = heroes
     );
   }
 
-  deleteHero(id:number){
+  deleteHero(id: number) {
+    //borrar Heroe
     this.heroService.deleteHero(id);
-
+    //Recargar lista de Heroes
     this.getHeroes();
   }
 }
